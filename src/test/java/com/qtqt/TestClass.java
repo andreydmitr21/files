25b9331f17e02fd36f3f3714dc9fc8086eed339e
andreydmitr21 <andreydmitr21@gmail.com>
2022-05-25 19:42:46 +0200

    first

--- src/test/java/com/qtqt/TestClass.java
+++ src/test/java/com/qtqt/TestClass.java
@@ -6,15 +6,24 @@ import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.collections.ContainExactTextsCaseSensitive;
 import com.codeborne.xlstest.XLS;
 import com.opencsv.CSVReader;
+import dev.failsafe.internal.util.Assert;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+import java.util.zip.ZipInputStream;
 
 import static com.codeborne.pdftest.PDF.containsText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static java.nio.file.Files.createFile;
+import static java.nio.file.Files.delete;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class TestClass {
@@ -66,10 +75,56 @@ public class TestClass {
 
     @Test
     public void csvParsingTest() throws Exception {
-        try (InputStream inps = cl.
-                getResourceAsStream("csv/test.csv")) {
-            CSVReader csv = new CSVReader(new InputStreamReader(inps));
+        try (
+                InputStream inps = cl.getResourceAsStream("csv/test.csv");
+                CSVReader csv = new CSVReader(new InputStreamReader(inps, StandardCharsets.UTF_8))
+        ) {
+            List<String[]> content = csv.readAll();
+            org.assertj.core.api.Assertions.assertThat(content).contains(
+                    new String[]{"Dmitry", " Tuchs"}
+            );
+        }
+    }
 
+    @Test
+    public void zipParsingTest() throws Exception {
+        String docxFile="q.docx";
+        try (
+                ZipInputStream inps = new ZipInputStream(
+                        cl.getResourceAsStream("zip/спряжение глаголов.docx.zip"))) {
+            ZipEntry entry;
+            ZipInputStream zipIn = new ZipInputStream(inps);
+            while ((entry = inps.getNextEntry()) != null) {
+                if (entry.getName().contentEquals("спряжение глаголов.docx")) {
+                    // read file
+                    String entryFileName = entry.getName();
+                    Path entryPath = Paths.get("/src/test/java/resources/TMP/",docxFile);
+                    System.out.println(entryPath.toString());
+
+                    // Create the entry file by creating necessary directories
+                    try {
+                        delete(entryPath);
+                    } catch (Exception e)
+                    {
+
+                    }
+                    createFile(entryPath);
+
+                    // Create an output stream to extract the contents of the
+                    // zip entry and write to the new file
+                    try (BufferedOutputStream bos = new BufferedOutputStream(
+                            new FileOutputStream(entryPath.toString()))) {
+                        byte[] buffer = new byte[1024];
+                        int count;
+                        while ((count = zipIn.read(buffer)) != -1) {
+                            bos.write(buffer, 0, count);
+                        }
+                    }
+
+                    return;
+                }
+            }
+            throw new Exception("no file "+myZip+ "inside zip");
         }
     }
 }
